39b5b7018b068d1793f8f3ced2243fb7757c39c6
ignarch <ignanttt@gmail.com>
2024-10-19 21:49:32 +0200

    UPDATED | string problem solved, fgets function for string input

---
+++ strings/user_data_collector.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+# include <string.h>
+
+int main()
+{
+    char user_name[30];
+
+    printf("Please enter your name: ");
+    // scanf("%s", user_name); // This will get just the string until the fist space
+    fgets(user_name, 30, stdin); // fgets gets the full string but has a \n at the end
+    user_name[strcspn(user_name, "\n")] = '\0'; // 'strcspn' will delete the '\n' character
+    printf("Hello '%s' nice to meet you\n", user_name);
+
+
+    return(0);
+}
