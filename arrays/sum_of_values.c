f5eb334fe98f4c2c06904047938b258e4254083a
ignarch <ignanttt@gmail.com>
2024-10-27 23:02:32 +0100

    UPDATED | another array problem solved

--- arrays/sum_of_values.c
+++ arrays/sum_of_values.c
@@ -1,10 +1,23 @@
 # include <stdio.h>
 # include <string.h>
 
-//defining macros...
-# define
+//defining macros for global variable values
+# define AGES 4
 
 int main()
 {
+	int people_ages[AGES]; // Value array with AGES value = 4
+	int i = 0; // This will act as a counter
+	int sum_of_ages = 0;
 
+	for(i; i<AGES; i++){
+		printf("Please enter the #%d age: ", i+1);
+		scanf("%d", &people_ages[i]); // This will go over each slot of the array and will append the value to it until it reaches the maximu
+
+		sum_of_ages += people_ages[i];	
+	}
+
+	printf("The total sum of ages is: %d\n", sum_of_ages);
+
+	return(0);
 }
