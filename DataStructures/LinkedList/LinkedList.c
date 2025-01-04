870a31d9ccb75572aeb9efc642bffc16340e6303
ntIgnant <ignanttt@gmail.com>
2025-01-04 23:40:36 +0100

    UPDATED| Started with linkedlist for Data structures

---
+++ DataStructures/LinkedList/LinkedList.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+typedef struct Node{
+	int data; // Data here will be a number
+	struct *Node next; // Pointer to the 'next node'
+}Node;
+
+int main(){
+	
+	
+	
+	return(0);
+}
