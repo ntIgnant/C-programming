3066b0faac4510dcd2a30a638888c5154ec6844a
ntIgnant <ignanttt@gmail.com>
2025-01-05 17:20:44 +0100

    UPDATED | LinkedList DS started

--- DataStructures/LinkedList/LinkedList.c
+++ DataStructures/LinkedList/LinkedList.c
@@ -10,7 +10,12 @@ typedef struct Node{
 
 int main(){
 	
-	
+    Node* head = (Node*)malloc(sizeof(Node)); // Head is always the first node of the Linked List
+    Node* second = (Node*)malloc(sizeof(Node));
+    Node* third = (Node*)malloc(sizeof(Node));
+
+    // Assign the values
+    //
 	
 	return(0);
 }
