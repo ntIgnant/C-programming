7880de735f8b66896619daefa50104395830878e
ntIgnant <ignanttt@gmail.com>
2025-01-07 21:27:53 +0100

    UPDATED | traversal function added

--- DataStructures/LinkedList/LinkedList3.c
+++ DataStructures/LinkedList/LinkedList3.c
@@ -11,15 +11,41 @@ typedef struct Node{
     struct Node* next; // Assign a value fr the next node
 }Node;
 
+void traversal(Node* head){
+    Node* tmp_node = head;
+    printf("Data Availeable:\n");
+    while(tmp_node != NULL){
+        printf("%s | %d | %s\n", tmp_node->name, tmp_node->age, tmp_node->city);
+        tmp_node = tmp_node->next;
+    }
+}
 
 int main(){
+
+    // Create the initial Nodes (head and tail)
     Node* head = (Node*)malloc(sizeof(Node));
     Node* tail = (Node*)malloc(sizeof(Node));
 
+    // Add the vaues of the initial Nodes
+    strcpy(head->name, "Kit0");
+    strcpy(head->city, "Loja");
+    head->age = 19;
+    head->next = tail; // Linkd the node to it's next
+
+    strcpy(tail->name, "ntIgnant");
+    strcpy(tail->city, "Quito");
+    tail->age = 19;
+    tail->next = NULL; // next-node is NULL for the tail (last node)
+
     // Add functions
 
-    // traversal()
+    // traversal() ...Pinrt out the data of the LinkedList
+    traversal(head);
+
     // addAtBeginning
     // addAtEnd
+    //Free dynamically allocated memory
+    free(head);
+    free(tail);
     return(0);
 }
