2fb7e5236de46a9913902c9415c77d08f3358d52
ntIgnant <ignanttt@gmail.com>
2025-01-07 21:40:44 +0100

    UPDATED | new function added | addAtBeginning function created

--- DataStructures/LinkedList/LinkedList3.c
+++ DataStructures/LinkedList/LinkedList3.c
@@ -11,6 +11,7 @@ typedef struct Node{
     struct Node* next; // Assign a value fr the next node
 }Node;
 
+// TRAVERSAL function
 void traversal(Node* head){
     Node* tmp_node = head;
     printf("Data Availeable:\n");
@@ -18,6 +19,16 @@ void traversal(Node* head){
         printf("%s | %d | %s\n", tmp_node->name, tmp_node->age, tmp_node->city);
         tmp_node = tmp_node->next;
     }
+}
+
+// ADDATBEGINNING function
+void addAtBeginning(char new_name, int new_age, char new_city){
+    Node* newHead = (Node*)malloc(sizeof(Node));
+    
+    strcpy(newHead->name, new_name);
+    strcpy(newHead->city, new_city);
+    newHead->age = new_age;
+    newHead->next = head; // The next-node of the new head will be the previous head
 }
 
 int main(){
@@ -43,6 +54,20 @@ int main(){
     traversal(head);
 
     // addAtBeginning
+
+    char tmp_name[50]; // Create temporary values to assign to the new nodes
+    int tmp_age;
+    char tmp_city[50];
+
+    printf("Name: ");
+    scanf("%s", tmp_name);
+    printf("Age: ");
+    scanf("%d", &tmp_age);
+    printf("City: ");
+    scanf("%s", tmp_city);
+
+    addAtBeginning(char name, int age, char city);
+
     // addAtEnd
     //Free dynamically allocated memory
     free(head);
