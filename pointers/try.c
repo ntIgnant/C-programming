f8014464f901d0c63229dbe4afc1d765286c7a40
ntIgnant <ignanttt@gmail.com>
2025-01-10 18:43:04 +0100

    UPDATED | printf val with pointer reference *

--- pointers/try.c
+++ pointers/try.c
@@ -5,7 +5,7 @@ int main(){
     int x = 10;
     int* px = &x;
 
-    printf("Number: %d | Address: %p\n", x, px);
+    printf("Number: %d | Address: %p | Number(pointer*): %d\n", x, px, *px);
 
     return(0);
 }
