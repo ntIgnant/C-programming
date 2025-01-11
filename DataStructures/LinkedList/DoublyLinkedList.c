3c6eab8f106d9393ce61ac4560ab1bf5e24e34a2
ntIgnant <ignanttt@gmail.com>
2025-01-11 20:01:52 +0100

    UPDATED | doubly linked list errors fixed | memory allocation fixed

--- DataStructures/LinkedList/DoublyLinkedList.c
+++ DataStructures/LinkedList/DoublyLinkedList.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Node{
-    struct Node* prev; // previous node for Doubly Linked List
-    int data;
-    struct Node* next;
+typedef struct Node {
+    struct Node* prev; // Previous node for Doubly Linked List
+    int data;          // Data stored in the node
+    struct Node* next; // Next node for Doubly Linked List
+} Node;
 
-}Node;
+int main() {
+    // Declare an array of pointers for 3 nodes
+    Node* node[3];
 
-int main(){
-    
-    // Dynamically allocate memory with a for loop (for 3 nodes)
-    for(int i = 0; i < 3; i++){
-        Node* node[i] = (Node*)malloc(sizeof(Node));
+    // Dynamically allocate memory for 3 nodes
+    for (int i = 0; i < 3; i++) {
+        node[i] = (Node*)malloc(sizeof(Node));
 
-        //check for memory allocation failure
-        if(node[i] == NULL){
+        // Check for memory allocation failure
+        if (node[i] == NULL) {
             printf("Memory allocation failure\n");
-            exit(1);
+            exit(1); // Exit program on failure
         }
-        printf("Allocation for 3 nodes was successfully done!\n");
     }
+    printf("Allocation for 3 nodes was successfully done!\n");
 
+    // Initialize the nodes
     node[0]->data = 0;
     node[0]->prev = NULL; // Head
     node[0]->next = node[1];
@@ -33,12 +35,21 @@ int main(){
     node[2]->data = 2;
     node[2]->prev = node[1];
     node[2]->next = NULL; // Tail
-    
+
+    // Print the doubly linked list
+    for (int i = 0; i < 3; i++) {
+        printf("Node %d: Data = %d, Prev = %p, Next = %p\n", 
+            i, 
+            node[i]->data, 
+            (void*)node[i]->prev, 
+            (void*)node[i]->next
+        );
+    }
 
     // Free memory
-    for(int i = 0; i < 3; i++){
+    for (int i = 0; i < 3; i++) {
         free(node[i]);
     }
 
-    return(0);
+    return 0;
 }
