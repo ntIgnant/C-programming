a6eca997d0bd70b6aca8f40f4176a20a03e1c65b
ntIgnant <ignanttt@gmail.com>
2025-01-12 13:48:32 +0100

    UPDATED | reverseTraversal | dynamically allocation for nodes done

--- DataStructures/LinkedList/reverseTraversal.c
+++ DataStructures/LinkedList/reverseTraversal.c
@@ -3,25 +3,39 @@
 #include <string.h>
 
 typedef struct Node{
-    structure Node* prev; // Revious Node bc Doubly LinkedList
+    struct Node* prev; // Revious Node bc Doubly LinkedList
     char name[20];
-    structure Node* next;
+    struct Node* next;
 }Node;
 
 // Reverse Traversal Function
 
 int main(){
-    int node_amount = 3;
+    const int node_amount = 3;
 
-    Node* node[]; // Create 10 instances for Node (10 different Nodes)
+    Node* node[node_amount]; // Create 10 instances for Node (10 different Nodes)
     
     for(int i = 0; i < node_amount; i++){
-        node[i] = (Node*)malloc(sizeof(Node));
+        node[i] = (Node*)malloc(sizeof(Node)); // Dynamically allocate memory for the nodes
         if(node[i] == NULL){
             printf("Memory Allocation Failed!\n");
         }
         printf("Node[%d] created\n", i);
     }
     
+    //Assign values and linkd the nodes
+    node[0]->prev = NULL; // Bc it's the head of the DLinkedList
+    strcpy(node[0]->name, "ntIgnant");
+    node[0]->next = node[1];
+
+    node[1]->prev = node[0];
+    strcpy(node[1]->name, "Kit0");
+    node[1]->next = node[2];
+
+    node[2]->prev = node[1];
+    strcpy(node[2]->name, "Oscar");
+    node[2]->next = NULL; // Bc it's the tail of the DLinkedList
+
+
     return(0);
 }
