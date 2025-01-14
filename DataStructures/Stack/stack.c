8bea54f98ddea85f9046a8b2f19c07bd89fda25e
ntIgnant <ignanttt@gmail.com>
2025-01-14 01:26:13 +0100

    UPDATED | LinkedList-based stack created | menu function created

--- DataStructures/Stack/stack.c
+++ DataStructures/Stack/stack.c
@@ -1,10 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-// Stack has 3 main functions ()
+typedef struct Node{
+    struct Node* next;
+    char name[20]; // Data for this LinkedList Based stack
+}Node;
+
+int menu(){
+    int menu_index;
+    printf("Menu for LinkedList-based stack...\n");
+
+    while(true){
+        printf("Please select one of the following (index): ");
+        scanf("%d", &menu_index);
+        if(menu_index < 0 || menu_index > 2){ // range 0 - 2 bc of just 3 functions in this program (push, pop, get)
+            printf("Please try again (out of range)\n");
+        }
+        else{
+            return(menu_index);
+        }
+    } 
+}
+
+// Stack has 3 main functions (push, pop, get...?)
+
+
+
 int main(){
-    
-    
     
     
     return(0);
