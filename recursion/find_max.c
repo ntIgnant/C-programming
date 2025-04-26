dccd8d6ab23ffa0ad56f936eb3987d83b39b9037
ntIgnant <ignanttt@gmail.com>
2025-04-26 11:44:21 +0200

    UPDATED | finx the maximum num in an array with recursion

---
+++ recursion/find_max.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int Find_Max(int i, char numAr[], int max, int ArLen){
+    if(i == ArLen){
+        return(max); // Return the maximum found number
+    }
+    else{
+        max = numAr[i]; //?
+    }
+
+
+}
+
+int main(){
+    
+    char num
+
+    return(0);
+}
