932d5bcdc7e717e14c2cd820fc6f6950b64a0c74
ntIgnant <ignanttt@gmail.com>
2025-05-11 23:18:41 +0200

    UPDATED | bruh

---
+++ pointers/swap.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void SwapNum(int *num1, int *num2){
+    
+}
+
+int main(){
+
+    int num1, num2;
+
+    int *pNum1 = &num1; // Assing the pointer to point to the memory address of num1
+    int *                    // Same for num2
+    
+    printf("Please write two numbers (space separated): ");
+    scanf("%d %d");
+
+    return 0;
+}
